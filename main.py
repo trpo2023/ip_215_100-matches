3b02f7a408721158d8f0ba198eacc24efdc39b29
Dima22824 <59305613+Dima22824@users.noreply.github.com>
2023-05-24 00:00:45 +0700

    main.py

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from intro import print_intro
-from take_matches import take_matches
-from switch_player import switch_player
-from result import print_result
-def play_game():
-    matches = 100
-    current_player = 1
-
-    while matches > 0:
-        matches = take_matches(matches, current_player)
-
-        if matches == 0:
-            print_result(current_player)
-        else:
-            current_player = switch_player(current_player)
-
-    print("Игра окончена.")
+from welcome import print_welcome_message
+from input import get_valid_input
+from player import switch_player
+from game import play_game
+def main():
+    print_welcome_message()
+    play_game()
 
 if __name__ == "__main__":
-    print_intro()
-    play_game()
+    main()
