84a11baf10dd3322ff8be6f532f0cabe7ff84092
Dima22824 <59305613+Dima22824@users.noreply.github.com>
2023-06-03 21:12:24 +0700

    test_borders.py

---
+++ test_borders.py
@@ -0,0 +1,55 @@
+import borders
+import unittest
+
+
+class LogicTests(unittest.TestCase):
+    def test_check_inputNum(self):
+        inputNum = 0
+        self.assertFalse(borders.inputNum(inputNum))
+
+        inputNum = 11
+        self.assertFalse(borders.inputNum(inputNum))
+
+        inputNum = 7
+        self.assertTrue(borders.inputNum(inputNum))
+
+        inputNum = 10
+        self.assertTrue(borders.inputNum(inputNum))
+
+    def test_check_inputNum2(self):
+        inputNum = 5
+        count = 4
+        self.assertFalse(borders.inputNum2(inputNum, count))
+
+        inputNum = 7
+        count = 1
+        self.assertFalse(borders.inputNum2(inputNum, count))
+
+        inputNum = 3
+        count = 6
+        self.assertTrue(borders.inputNum2(inputNum, count))
+
+        inputNum = 9
+        count = 26
+        self.assertTrue(borders.inputNum2(inputNum, count))
+
+    def test_check_compNum(self):
+        compNum = 9
+        count = 4
+        self.assertFalse(borders.pcNum(compNum, count))
+
+        compNum = 7
+        count = 6
+        self.assertFalse(borders.pcNum(compNum, count))
+
+        compNum = 3
+        count = 6
+        self.assertTrue(borders.pcNum(compNum, count))
+
+        compNum = 7
+        count = 15
+        self.assertTrue(borders.pcNum(compNum, count))
+
+
+if __name__ == "__main__":
+    unittest.main()
