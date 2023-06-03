d1715587ff696ff167c0086a3970478a5ee4afb3
Dima22824 <59305613+Dima22824@users.noreply.github.com>
2023-06-03 20:42:22 +0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from welcome import print_welcome_message
-from input import get_valid_input
-from player import switch_player
-from game import play_game
+from game import play
 def main():
-    print_welcome_message()
-    play_game()
+    play()
 
 if __name__ == "__main__":
     main()
