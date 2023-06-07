846a4d8cf4912d8275403163110946c8386e7f57
Dima22824 <59305613+Dima22824@users.noreply.github.com>
2023-06-07 20:57:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from game import play
-def main():
-    play()
+from matches_game import printDescription, playGame
 
-if __name__ == "__main__":
-    main()
+while True:
+    print("\n-------------| 100 спичек |-------------")
+    print("1. Описание игры")
+    print("2. Игра")
+    print("3. Выход")
+    selectedOption = int(input())
+
+    if selectedOption == 1:
+        printDescription()
+    elif selectedOption == 2:
+        playGame()
+    elif selectedOption == 3:
+        break
+    else:
+        print("Выберите корректный пункт меню")
